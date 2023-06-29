db06ebf9fff47e58713e024dcaa7e250b7f4c018
PavanTamrakar <thepavantamrakar@gmail.com>
2023-06-29 21:50:40 +0530

    logic

---
+++ src/nSCLogic.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract nSCLogic {
+    function depositAndMint () external {}
+
+    function deposit () external {}
+
+    function redeemDeposit () external {}
+
+    function redeem () external {}
+
+    function mintSC () external {}
+
+    function burnSC () external {}
+
+    function hetHealthFactor () external {}
+}
